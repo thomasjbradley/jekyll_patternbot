e111220d71e45589f08cbe7ea9861a4f524f49b9
Thomas J Bradley <hey@thomasjbradley.ca>
2019-01-01 17:08:37 -0500

    Cleanup & comment unused code

--- lib/jekyll-patternbot-plugins.rb
+++ lib/jekyll-patternbot-plugins.rb
@@ -9,10 +9,10 @@ module JekyllPatternbot
   require 'jekyll-patternbot-plugins/file-helpers'
   require 'jekyll-patternbot-plugins/jekyll-helpers'
   require 'jekyll-patternbot-plugins/patterns'
+  require 'jekyll-patternbot-plugins/hooks'
   require 'jekyll-patternbot-plugins/pattern-html'
   require 'jekyll-patternbot-plugins/pattern-css'
   require 'jekyll-patternbot-plugins/pattern-js'
-  require 'jekyll-patternbot-plugins/hooks'
   require 'jekyll-patternbot-plugins/generators'
   require 'jekyll-patternbot-plugins/tags'
 end
--- lib/jekyll-patternbot-plugins/generators.rb
+++ lib/jekyll-patternbot-plugins/generators.rb
@@ -1,36 +1,16 @@
 module JekyllPatternbot
 
-  class PatternSingle < Jekyll::Generator
-    priority :normal
+  # class PatternSingle < Jekyll::Generator
+  #   priority :normal
 
-    def initialize(site)
-      @site = site
-    end
+  #   def initialize(site)
+  #     @site = site
+  #   end
 
-    def generate(site)
-      @site = site
-      @site.pages.concat(patterns_to_pages)
-    end
+  #   def generate(site)
+  #     @site = site
+  #   end
 
-    def patterns_to_pages
-      html_files.map { |doc| page_from_static_file(doc) }
-    end
-
-    def html_files
-      @site.documents.each.select { |doc| doc.relative_path.start_with? Config['patternbot']['settings']['source'] and doc.extname == '.html' }
-    end
-
-    def page_from_static_file(static_file)
-      base = static_file.instance_variable_get('@base')
-      dir  = static_file.instance_variable_get('@dir')
-      name = static_file.instance_variable_get('@name')
-      data = static_file.instance_variable_get('@data')
-      data['layout'] = 'pattern-single'
-      page = Jekyll::Page.new(@site, base, dir, name)
-      page.data = data
-      page
-    end
-
-  end
+  # end
 
 end
