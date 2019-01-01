d930acf0bdc64c9804da8ea701dc42e6abeb2f6b
Thomas J Bradley <hey@thomasjbradley.ca>
2019-01-01 11:38:19 -0500

    Remove the initial testing stuff that I’m not currently using

--- spec/jekyll-patternbot_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Jekyll::Patternbot do
-  it "has a version number" do
-    expect(Jekyll::Patternbot::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
