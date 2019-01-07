9f69259f2a69c1b5c552c737f5439c3cd8b3ba4b
Thomas J Bradley <hey@thomasjbradley.ca>
2019-01-06 20:12:32 -0500

    Add a caching system and merge local _config with user _config

--- _plugins/lib/jekyll_patternbot_plugins/config.rb
+++ _plugins/lib/jekyll_patternbot_plugins/config.rb
@@ -1,5 +1,10 @@
 module JekyllPatternbot
   Config = YAML.load_file File.expand_path('../../../_config.yml', __dir__)
   CSSColorNames = YAML.load_file File.new File.expand_path('css_color_names.json', __dir__)
+  class << self; attr_accessor :PatternbotCache; end
   class << self; attr_accessor :PatternbotData; end
+
+  Jekyll::Hooks.register :site, :after_init do |site|
+    Config.deep_merge! site.config
+  end
 end
