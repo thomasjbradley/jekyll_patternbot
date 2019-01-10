0413b6bcb78b6c2f369a01ddf123d09e75787b9e
Thomas J Bradley <hey@thomasjbradley.ca>
2019-01-10 16:35:27 -0500

    Allow designers to provide rationales for fonts & colours

--- _plugins/lib/jekyll_patternbot_plugins/filters/text.rb
+++ _plugins/lib/jekyll_patternbot_plugins/filters/text.rb
@@ -5,6 +5,16 @@ module JekyllPatternbot
       PatternHelper.slug_to_words(s).unicode_titlecase
     end
 
+    def has_pattern_rationale(id, *namespaces)
+      namespaces.push(id)
+      Config['patternbot']['rationales'].key? namespaces.join('.')
+    end
+
+    def get_pattern_rationale(id, *namespaces)
+      namespaces.push(id)
+      Config['patternbot']['rationales'][namespaces.join('.')]
+    end
+
   end
 end
 
