d345a650a5901ef054e00070e4e0bcd6ad6ce4d5
Thomas J Bradley <hey@thomasjbradley.ca>
2019-01-10 20:40:55 -0500

    Add URL insertion for the Web Dev Tools

--- _plugins/lib/jekyll_patternbot_plugins/filters/text.rb
+++ _plugins/lib/jekyll_patternbot_plugins/filters/text.rb
@@ -15,6 +15,13 @@ module JekyllPatternbot
       Config['patternbot']['rationales'][namespaces.join('.')]
     end
 
+    def sub_web_dev_tool_urls(text)
+      text.gsub!(/\+\+modulifier\-settings\-url\+\+/, PatternbotData[:css][:modulifier][:url]) if PatternbotData[:css][:modulifier]
+      text.gsub!(/\+\+gridifier\-settings\-url\+\+/, PatternbotData[:css][:gridifier][:url]) if PatternbotData[:css][:gridifier]
+      text.gsub!(/\+\+typografier\-settings\-url\+\+/, PatternbotData[:css][:typografier][:url]) if PatternbotData[:css][:typografier]
+      text
+    end
+
   end
 end
 
